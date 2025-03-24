35a148ce6a9934c7437692ce40d8fd089f471582
Mehmet Ali <malitopal113@gmail.com>
2025-03-24 17:08:28 +0100

    middlaware basics

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})      
+})     
+
+
+const mw1 = (req, res, next) => {
+    console.log('Middleware 1');
+    next();
+}
+app.use(express.static('public'));
+app.use(mw1);
+
+
 
 app.get('/' , (req, res) => {
-        console.log('Server is running');
-        const photo = {
-            id: 1,
-            title: 'Node.js',   
-            descrition: 'Node.js is a runtime environment for JavaScript that runs on the server.',
-            url: 'https://nodejs.org/',
-        }
 
-        const url = req.url;
+        res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 
-        if(url === "/index" || url === "/") {
-            res.send(photo);      
+
+
+    
+        // console.log('Server is running');
+        // const photo = {
+        //     id: 1,
+        //     title: 'Node.js',   
+        //     descrition: 'Node.js is a runtime environment for JavaScript that runs on the server.',
+        //     url: 'https://nodejs.org/',
+        // }
+
+        // const url = req.url;
+
+        // if(url === "/index" || url === "/") {
+        //     res.send(photo);      
     }
-}
 )
